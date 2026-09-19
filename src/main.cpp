353457442be8a4cf25fa72119b4bc6ba4c67377a
DaniArellano0348 <160549791+DaniArellano0348@users.noreply.github.com>
2026-09-19 05:43:39 +0000

    Configurar ventana inicial de SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "A* SFML iniciado correctamente." << std::endl;
+
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 600;
+
+    sf::RenderWindow window(
+        sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+        "A* Pathfinding - C++ SFML"
+    );
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::White);
+
+        window.display();
+    }
 
     return 0;
 }
